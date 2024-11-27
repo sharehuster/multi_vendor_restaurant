862d59bd2e4886931ab02b7424ba6abc26fb4b71
sharehuster <dungpham12399@gmail.com>
2024-11-27 22:14:07 +0700

    setup super user create home view (not apply static file) create some folder static

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello")
+    # return HttpResponse("Hello")
+    return render(request, 'home.html')
