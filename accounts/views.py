d9f7ae6fb054e43e1876ed74392d98621af40f6e
sharehuster <dungpham12399@gmail.com>
2024-12-04 22:59:40 +0700

    custom form for register user

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
 
 def registerUser(request):
-    form = UserForm()
+    if request.method == "POST":
+        print(request.POST)
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            form.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm()
+
     context = {
         'form': form,
     }
