5feac83c4ed8b3507d908912b59eea9984d29fc9
sharehuster <dungpham12399@gmail.com>
2024-12-10 23:34:09 +0700

    forgot password -> reset password validate -> reset password

--- accounts/utils.py
+++ accounts/utils.py
@@ -18,11 +18,10 @@ def detectUser(user):
         redirectUrl = '/admin'
         return redirectUrl
     
-def send_verification_email(request, user):
+def send_verification_email(request, user, mail_subject, email_template):
     from_email = settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
-    mail_subject = "Please activate your account"
-    message = render_to_string('accounts/emails/account_verification_email.html', {
+    message = render_to_string(email_template, {
         'user': user,
         'domain': current_site,
         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -30,7 +29,8 @@ def send_verification_email(request, user):
     })
     to_email = user.email
     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.content_subtype = "html"
     try:
         mail.send()
     except Exception as e:
-        print("send mail error:",e)
+        print("send mail error:",e) 
