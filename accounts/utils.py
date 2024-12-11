d982716a21141c8350a871cd0978584e20ba79ab
sharehuster <dungpham12399@gmail.com>
2024-12-11 22:37:58 +0700

    admin approve vendor -> send email

--- accounts/utils.py
+++ accounts/utils.py
@@ -34,3 +34,20 @@ def send_verification_email(request, user, mail_subject, email_template):
         mail.send()
     except Exception as e:
         print("send mail error:",e) 
+
+
+
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    if(isinstance(context['to_email'], str)):
+        to_email = []
+        to_email.append(context['to_email'])
+    else:
+        to_email = context['to_email']
+    mail = EmailMessage(mail_subject, message, from_email, to=to_email)
+    mail.content_subtype = "html"
+    try:
+        mail.send()
+    except Exception as e:
+        print("send mail error:",e) 
